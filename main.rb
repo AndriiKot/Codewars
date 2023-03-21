056506911bb7b4056b321ccdff2635562cdfc0fb
AndriiKot <a_kocyuba@ukr.net>
2023-03-21 21:28:37 +0100

    format lesson 2

--- main.rb
+++ main.rb
@@ -1,61 +1,37 @@
-﻿=begin
- JS
- let str = "String 1"   // undefined
- str                    // "String 1
-str.length             //  8
-"Hello js!".length     //  9
-str[0]                 // "S"
-str[str.length - 1]    // "1"
-str.slice(1)           // "tring 1"
-str.slice(1,3)         // "tr"
-str.toLowerCase()      // "string 1"
-console.log(str)       // "String 1" !!!
-str.toUpperCase()      // "STRING 1"
-console.log(str)       // "String 1" !!!
-
-
-let text = "   hElLO jS!    "
-text.length                                 // 16
-text.trim()   
-                                           // "hELLO jS!"
-let str1 = text.toLowerCase().trim()      // "hello js!"
-
-str1  = str1[0].toUpperCase() + str1.slice(1)
-console.log(str1)                      // "Hello js!"
-=end
-str = 'String 1'  # "String 1"
-str.size          # 8
-p "Hello Ruby!".size   # 11
-str[0]                 # "S"
-str[-1]                # "1"
+﻿
+str = 'String 1'        # "String 1"
+str.size                # 8
+"Hello Ruby!".size      # 11
+str[0]                  # "S"
+str[-1]                 # "1"
 str[1..-1]              # "tring 1"
-str[1,3]              # "tri"
+str[1,3]                # "tri"
 
 joe = 'joe'
-p joe.capitalize
-p joe
-joe.capitalize!
-p joe
+p joe.capitalize        # "Joe"
+p joe                   # "joe" !!!
+joe.capitalize!         # "Joe"
+p joe                   # "Joe" !!!
 
 cat = "CAT"
- cat.downcase
- p cat
- cat.downcase!
- p cat
+cat.downcase            # "cat"
+p cat                   # "CAT" !!!
+cat.downcase!           # "cat"
+p cat                   # "cat" !!!
 
- ibm = 'ibm'
- ibm.upcase
- p ibm
- ibm.upcase!
- p ibm
+ ibm = 'ibm'            
+ ibm.upcase             # "IBM"
+ p ibm                  # "ibm" !!!
+ ibm.upcase!            # "IBM"
+ p ibm                  # "IBM"
 
- oxi = "OXI"
+ oxi = "OXI"            
  oxi.capitalize!
- p oxi
+ p oxi                  # "Oxi" !!!
 
  text = "    hELo rUBY!    "
  text.strip!.capitalize!
- p text
+ p text                   # "Hello ruby!"
 
 
 
