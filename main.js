0f0e5ebeac8760a6dc2599cd7b80b6210f5e5d5c
AndriiKot <a_kocyuba@ukr.net>
2023-03-29 08:39:42 +0200

    Creation Lesson 5 'if else'

---
+++ main.js
@@ -0,0 +1,44 @@
+﻿const calculateSubtotal = (goods) => {
+  let amount = 0;
+
+// Variation 1
+  for (const item of goods) {
+   if(item.price < 0) throw 'Negative price';
+     else amount += item.price;
+   }
+  return amount;
+};
+
+// Variation 2.0
+  if (item.price < 0) throw 'Negative price';
+     amount += item.price;
+
+  if (item.price < 0) {
+    throw 'Negative price';
+  }
+  amount += item.price;
+
+// Variation 2.1
+  if(item.price < 0) return;
+    amount += item.price;
+
+  if (item.price < 0) {
+    return;
+  }
+  amount += item.price;
+
+// BAD
+   if (item.price > 0) return true;   // !!!  bad
+     return false;                   //  !!!  bad
+
+// Good
+  return item.price > 0;
+
+
+
+
+
+
+
+
+
