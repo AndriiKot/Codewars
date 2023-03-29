9f49b993bed6a58477260e488e52b3c8f0227c70
AndriiKot <a_kocyuba@ukr.net>
2023-03-29 09:14:46 +0200

    add operators and tern. oper:

--- main.js
+++ main.js
@@ -27,12 +27,55 @@
   }
   amount += item.price;
 
+
+
 // BAD
    if (item.price > 0) return true;   // !!!  bad
      return false;                   //  !!!  bad
 
 // Good
   return item.price > 0;
+
+
+
+
+// BAD
+  if (order[groupName] && order[groupName].length > 0) {   // bad
+    total += calculateTotal(order[groupName]);
+    count += order[groupName].length;
+  }
+// Good
+  if (goods && goods.length > 0) {
+    total += calculateTotal(goods);
+    count += goods.lenght;              // ??
+  }
+// Very good
+  const goods = order[groupName];
+  if (goods) {
+   const len = goods.lenght;
+   if (len > 0) {
+     total += calculateTotal(goods);
+     count += len;
+    }
+  }
+// Very good
+  const goods = order[groupName];
+  if (!goods) return;
+  const len = goods.length;
+  if (len > 0) {
+   total += calculateTotal(goods);
+   count += len;
+  }
+
+// TER.
+  amount += item.price < 0 ? 0 : item.price;
+  amount += item.price > 0 ? item.price : 0;
+
+
+
+  
+
+
 
 
 
