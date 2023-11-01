c234672021f437afae65f3f053a95418d15d6723
AndriiKot <a_kocyuba@ukr.net>
2023-11-01 09:57:19 +0100

    task: 'Understanding Uninitialized Variables'

---
+++ freecodecamp/learn JS/Basic Javascript/_7_Understanding Uninitialized Variables/script.js
@@ -0,0 +1,13 @@
+﻿// Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+
+// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+a = 5;
+b = 10;
+c = "I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
