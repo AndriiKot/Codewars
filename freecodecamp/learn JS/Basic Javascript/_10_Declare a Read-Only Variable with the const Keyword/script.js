7d526ed4f9669725ab203b1528d62ee645b75e9d
AndriiKot <a_kocyuba@ukr.net>
2023-11-01 10:15:55 +0100

    task: 'Declare a Read-Only Variable with the const Keyword'

---
+++ freecodecamp/learn JS/Basic Javascript/_10_Declare a Read-Only Variable with the const Keyword/script.js
@@ -0,0 +1,11 @@
+﻿/*
+Change the code so that all variables are declared using let or const.
+ Use let when you want the variable to change, and const when you want the variable to remain constant.
+ Also, rename variables declared with const to conform to common practices.
+ Do not change the strings assigned to the variables.
+*/
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
