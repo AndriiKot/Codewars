d2c354a9e1d38570cda231ba4de29e0dc2182f84
AndriiKot <a_kocyuba@ukr.net>
2023-11-01 09:50:30 +0100

    task: 'Declare String Variables'

---
+++ freecodecamp/learn JS/Basic Javascript/_6_Declare String Variables/script.js
@@ -0,0 +1,4 @@
+﻿// Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+var myFirstName = 'Andrii';
+var myLastName = 'Kotsiuba';
