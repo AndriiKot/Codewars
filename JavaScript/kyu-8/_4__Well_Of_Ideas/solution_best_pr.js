4283d0dc4e0d5aba958bbf85d340436eb4625ee4
AndriiKot <a_kocyuba@ukr.net>
2024-04-17 18:21:13 +0200

    kyu-8 JS 'Well of Ideas'

---
+++ JavaScript/kyu-8/_4__Well_Of_Ideas/solution_best_pr.js
@@ -0,0 +1,25 @@
+const well = (x) => {
+  const good_count = x.filter((x) => x == "good").length;
+  return good_count < 1
+    ? "Fail!"
+    : good_count < 3
+    ? "Publish!"
+    : "I smell a series!";
+};
+
+function well(x) {
+  const count = x.reduce((s, v) => s + (v == "good"), 0);
+  return count ? (count > 2 ? "I smell a series!" : "Publish!") : "Fail!";
+}
+
+function well(x) {
+  switch (x.filter((i) => i === "good").length) {
+    case 0:
+      return "Fail!";
+    case 1:
+    case 2:
+      return "Publish!";
+    default:
+      return "I smell a series!";
+  }
+}
