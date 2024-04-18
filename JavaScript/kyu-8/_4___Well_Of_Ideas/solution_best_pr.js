3580df714aa6671d6459a974e71b8faaf491bf31
AndriiKot <a_kocyuba@ukr.net>
2024-04-18 23:04:55 +0200

    kyu-8 JS fixed bag

--- JavaScript/kyu-8/_4___Well_Of_Ideas/solution_best_pr.js
+++ JavaScript/kyu-8/_4___Well_Of_Ideas/solution_best_pr.js
@@ -1,4 +1,4 @@
-const well = (x) => {
+const well1 = (x) => {
   const good_count = x.filter((x) => x == "good").length;
   return good_count < 1
     ? "Fail!"
@@ -7,12 +7,12 @@ const well = (x) => {
     : "I smell a series!";
 };
 
-function well(x) {
+function well2(x) {
   const count = x.reduce((s, v) => s + (v == "good"), 0);
   return count ? (count > 2 ? "I smell a series!" : "Publish!") : "Fail!";
 }
 
-function well(x) {
+function well3(x) {
   switch (x.filter((i) => i === "good").length) {
     case 0:
       return "Fail!";
