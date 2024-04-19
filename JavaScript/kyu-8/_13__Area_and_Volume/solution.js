fa34f05e34d8f91f97b793a411919cae6e6e91ba
AndriiKot <a_kocyuba@ukr.net>
2024-04-19 12:18:09 +0200

    kyu-8 JS 'Area and Volume'

---
+++ JavaScript/kyu-8/_13__Area_and_Volume/solution.js
@@ -0,0 +1,9 @@
+const getSize = (width, height, depth) => {
+  const surfaceArea = 2 * (depth * width + depth * height + width * height);
+  return [surfaceArea,width * height * depth]
+};
+
+// Sample Tests:
+console.log(getSize(4, 2, 6));
+console.log(getSize(5, 5, 5));
+console.log(getSize(9, 7, 2));
