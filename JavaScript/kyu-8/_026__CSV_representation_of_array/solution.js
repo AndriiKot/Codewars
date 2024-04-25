dfd1843d40ad6e70799116696a7e964c39d3537b
AndriiKot <a_kocyuba@ukr.net>
2024-04-25 10:44:33 +0200

    kyu-8 JS  'CSV representation of array'

---
+++ JavaScript/kyu-8/_026__CSV_representation_of_array/solution.js
@@ -0,0 +1,11 @@
+function toCsvText(array) {
+  let csv = ''
+  array.forEach((element) => {
+    csv += element.join(',') + '\n'
+  })
+  return csv.substring(0, csv.length - 1)
+}
+
+// Tests
+toCsvText([[0, 1, 2, 3, 4], [10, 11, 12, 13, 14], [20, 21, 22, 23, 24], [30, 31, 32, 33, 34]]);
+
