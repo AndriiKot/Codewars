1b3c335dfd9c6e3caaa1054eb49f76615a5441ec
AndriiKot <a_kocyuba@ukr.net>
2024-04-27 23:30:11 +0200

    __TOP__ JS kyu-8 'I love you a little ...'

---
+++ JavaScript/__TOP__/_8-kyu__I_love_you_a_little__/solution_best_pr.js
@@ -0,0 +1,24 @@
+const phrases = [
+  "I love you",
+  "a little",
+  "a lot",
+  "passionately",
+  "madly",
+  "not at all",
+];
+
+function howMuchILoveYou1(n) {
+  return phrases[(n - 1) % phrases.length];
+}
+
+function howMuchILoveYou2(nbPetals) {
+  let phrase = {
+    0: "not at all",
+    1: "I love you",
+    2: "a little",
+    3: "a lot",
+    4: "passionately",
+    5: "madly",
+  };
+  return phrase[nbPetals % 6];
+}
