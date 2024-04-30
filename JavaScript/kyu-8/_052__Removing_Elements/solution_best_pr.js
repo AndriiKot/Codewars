485664b4f2ac54f103b0e2d2f26296f973976359
AndriiKot <a_kocyuba@ukr.net>
2024-04-30 19:59:30 +0200

    kyu-8 JS  'Removing Elements'

---
+++ JavaScript/kyu-8/_052__Removing_Elements/solution_best_pr.js
@@ -0,0 +1,13 @@
+function removeEveryOther1(arr) {
+  return arr.filter(function (elem, index) {
+    return index % 2 === 0;
+  });
+}
+
+function removeEveryOther2(arr) {
+  return arr.filter((_, i) => !(i & 1));
+}
+
+console.log(removeEveryOther1([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+console.log(removeEveryOther2([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+
