a262326e8ac47660ea0a22d4e8dcc10aef3e2a31
AndriiKot <a_kocyuba@ukr.net>
2024-05-09 20:19:17 +0200

    kyu-8 JS 'Are You Playing Banjo'

---
+++ JavaScript/kyu-8/_073__Are_You_Playing_Banjo/solution_best_pr.js
@@ -0,0 +1,13 @@
+function areYouPlayingBanjo1(name) {
+  let answer = "";
+  if (/[rR]/.test(name[0])) {
+    answer = name + " plays banjo";
+  } else {
+    answer = name + " does not play banjo";
+  }
+  return answer;
+}
+
+function areYouPlayingBanjo2(name) {
+  return name + (/^r/i.test(name) ? " plays " : " does not play ") + "banjo";
+}
