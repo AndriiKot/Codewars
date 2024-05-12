aae4784aa1e1d9978c42d03d8848b878ba542a59
AndriiKot <a_kocyuba@ukr.net>
2024-05-12 13:35:40 +0200

    kyu-8 JS 'Find Min and Max'

---
+++ JavaScript/kyu-8/_088__Find_Min_and_Max/solution.js
@@ -0,0 +1,11 @@
+var min = function (list) {
+  return list.reduce((a, b) => a > b ? b : a);
+};
+
+var max = function (list) {
+  return list.reduce((a, b) => a < b ? b : a);
+};
+
+
+console.log(min([4, 6, 2, 1, 9, 63, -134, 566]));
+console.log(max([4, 6, 2, 1, 9, 63, -134, 566]));
