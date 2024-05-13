8f92d766c88731b7f9b33b81d2b884e865a73ff5
AndriiKot <a_kocyuba@ukr.net>
2024-05-13 15:21:17 +0200

    __TOP__ JS  kyu-8

---
+++ JavaScript/__TOP__/_8-kyu__Double_Char/solution_best_pr.js
@@ -0,0 +1,12 @@
+function doubleChar1(str) {
+  return str.replace(/(.)/g, "$1$1");
+}
+
+
+function doubleChar2(str) {
+  let doubleString = "";
+  for (char of str) {
+    doubleString += (char + char);
+  }
+  return doubleString;
+}
