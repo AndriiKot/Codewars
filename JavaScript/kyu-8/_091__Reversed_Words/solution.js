eded5763b33077d320f8e2c0613536c9fbcac7fd
AndriiKot <a_kocyuba@ukr.net>
2024-05-13 10:33:55 +0200

    kyu-8 JS 'Area or Perimeter', 'Grasshopper Grade book', 'Reversed Words'

---
+++ JavaScript/kyu-8/_091__Reversed_Words/solution.js
@@ -0,0 +1,17 @@
+function reverseWords(str) {
+  let newString = "";
+
+  const arrWords = str.split(" ");
+
+  newString += " " + arrWords.pop();
+
+  while (arrWords.length > 0) {
+    newString += " " + arrWords.pop();
+  }
+
+  return newString.trim();
+}
+
+console.log(
+  reverseWords("The greatest victory is that which requires no battle")
+);
