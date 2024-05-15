8ddfa14cae1e6470b905beb2cecaf77656f16c96
AndriiKot <a_kocyuba@ukr.net>
2024-05-15 10:34:01 +0200

    __TOP__ JS  kyu-8

---
+++ JavaScript/__TOP__/_8-kyu__Cat_years_Dog_years/solution_best_pr.js
@@ -0,0 +1,20 @@
+const humanYearsCatYearsDogYears1 = (humanYears) => [
+  humanYears,
+  (humanYears - 1 ? 16 : 11) + 4 * humanYears,
+  (humanYears - 1 ? 14 : 10) + 5 * humanYears,
+];
+
+var humanYearsCatYearsDogYears = function (humanYears) {
+  const humYears = humanYears;
+  let dogYears = 0;
+  let catYears = 0;
+  while (humanYears > 0) {
+    humanYears == 1
+      ? ((catYears += 15), (dogYears += 15))
+      : humanYears == 2
+      ? ((catYears += 9), (dogYears += 9))
+      : ((catYears += 4), (dogYears += 5));
+    humanYears -= 1;
+  }
+  return [humYears, catYears, dogYears];
+};
