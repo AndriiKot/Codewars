6d55da8423b332c161844211a19bf1b636431546
AndriiKot <a_kocyuba@ukr.net>
2024-05-20 19:22:59 +0200

    kyu-8 JS 'Triple Trouble'

---
+++ JavaScript/kyu-8/_159__Triple_Trouble/solution_best_pr.js
@@ -0,0 +1,8 @@
+function tripleTrouble(one, two, three) {
+  let newString = "";
+  const len = one.length;
+  for (let i = 0; i < len; i++) {
+    newString += one[i] + two[i] + three[i];
+  }
+  return newString;
+}
