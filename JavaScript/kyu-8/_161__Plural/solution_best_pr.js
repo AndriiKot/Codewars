adfd983c1d89c82a4369ff4d72cf9e102f622d9d
AndriiKot <a_kocyuba@ukr.net>
2024-05-20 22:35:48 +0200

    kyu-8 JS 'Plural'

--- JavaScript/kyu-8/_161__Plural/solution_best_pr.js
+++ JavaScript/kyu-8/_161__Plural/solution_best_pr.js
@@ -1,3 +1,10 @@
-function plural(n) {
-  return n !== 1;
+function correctTail1(bod, tail) {
+  return bod[bod.length - 1] === tail;
 }
+
+function correctTail2(body, tail) {
+  const sub = body.substr(body.length - tail.length);
+  return sub == tail;
+}
+
+
