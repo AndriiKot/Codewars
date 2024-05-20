9b5f7a9a24bfb44fc9859b0ff0b9ae26990be075
AndriiKot <a_kocyuba@ukr.net>
2024-05-20 18:26:08 +0200

    kyu-8 JS 'Capitalization and Mutab' and 'A wolf in sheep is clothing'

---
+++ JavaScript/kyu-8/_152__Capitalization_and_Mutab/solution_best_pr.js
@@ -0,0 +1,6 @@
+const capitalizeWord1 = (word) =>
+  word.replace(word.charAt(0), word.charAt(0).toUpperCase());
+
+function capitalizeWord(word) {
+  return word[0].toUpperCase() + word.slice(1);
+}
