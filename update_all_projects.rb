195204a2e0e54c3f5933c43659570f790bafd1f3
AndriiKot <a_kocyuba@ukr.net>
2024-05-25 16:19:49 +0200

    CodeWars   !!! !!!

---
+++ update_all_projects.rb
@@ -0,0 +1,11 @@
+﻿projects = {
+ SQL: 'https://github.com/AndriiKot/SQL__CodeWars.git',
+}
+
+update = "git subtree pull --prefix "
+
+projects.each do |folder, git_repo| 
+    system(`#{update}#{folder} #{git_repo} main`)
+end
+
+system(`git push`)
