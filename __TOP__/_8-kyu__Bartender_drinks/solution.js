60fa3fc511e0d5dcbfdde604846a85306be0b4df
AndriiKot <a_kocyuba@ukr.net>
2024-05-27 20:07:33 +0200

    JavaScript CodeWars 8-kyu __TOP__

---
+++ __TOP__/_8-kyu__Bartender_drinks/solution.js
@@ -0,0 +1,13 @@
+function getDrinkByProfession(param) {
+  getDrink = {
+    jabroni: "Patron Tequila",
+    "school counselor": "Anything with Alcohol",
+    programmer: "Hipster Craft Beer",
+    "bike gang member": "Moonshine",
+    politician: "Your tax dollars",
+    rapper: "Cristal",
+  };
+  return getDrink[param.toLowerCase()] || "Beer";
+}
+
+console.log(getDrinkByProfession("jabrOni"));
