095a9d09f647e13423d2cf796bbbf6a701705614
AndriiKot <a_kocyuba@ukr.net>
2024-05-28 15:54:13 +0200

    JavaScript CodeWars 8-kyu __TOP__

---
+++ __TOP__/_8-kyu__Return_the_day/solution_best_pr.js
@@ -0,0 +1,13 @@
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+function whatday(num) {
+  return weekdays[num - 1] || "Wrong, please enter a number between 1 and 7";
+}
