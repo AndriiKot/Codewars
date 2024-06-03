196ee00aadcb2b1cf4bcd551f0180930a3fe1543
AndriiKot <a_kocyuba@ukr.net>
2024-06-03 19:40:35 +0200

    JavaScript CodeWars 8-kyu 'Remove n exclamation marks in the sentence from left to right'

---
+++ kyu-8/_213__Remove_n_exclamation_marks_in_the_sentence_from_left_to_right/solution_best_pr.js
@@ -0,0 +1,22 @@
+function remove1(s, n) {
+  for (var i = 0; i < n; i++) s = s.replace("!", "");
+  return s;
+}
+
+// My solution
+function remove2(str, n) {
+  let count = 0;
+  let newStr = "";
+  const len = str.length;
+  for (let i = 0; i < len; i++) {
+    if (str[i] == "!" && count < n) {
+      count++;
+    } else {
+      newStr += str[i];
+    }
+  }
+  return newStr;
+}
+
+
+
