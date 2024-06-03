b0579eb353316498f2690ce6f31d8d73bae7605b
AndriiKot <a_kocyuba@ukr.net>
2024-06-03 16:10:49 +0200

    SQL Codewars 'Current Status' Fixed bag

--- SQL/kyu-8/_50__Surface_Area_and_Volume_of_a_Box/solution.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  width,
-  height,
-  depth,
-  (height * depth + width * depth + width * height) * 2 AS area,
-  (width * height * depth) AS volume
-FROM
-  box
-ORDER BY area, volume, width, height;
