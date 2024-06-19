170d40b1cd4b70ccc60b2593e26c9b6e59c66c0f
AndriiKot <a_kocyuba@ukr.net>
2024-06-19 10:48:18 +0200

    JavaScript CodeWars 'Current Status ' or 'README.md update

---
+++ kyu-8/_232__Tip_Calculator/solution.js
@@ -0,0 +1,18 @@
+function calculateTip(amount, rating) {
+  rating = rating.toLowerCase();
+  // const onePercent = amount ? amount / 100.0 : +0;
+  if (typeof amount === "number" && !isNaN(amount)) {
+    const tips = {
+      terrible: 0,
+      poor: amount * 0.05,
+      good: amount * 0.1,
+      great: amount * 0.15,
+      excellent: amount * 0.2,
+    };
+    return tips[rating] ? Math.ceil(tips[rating]) : 0;
+  } else {
+    return "Rating not recognised";
+  }
+}
+
+console.log(calculateTip(0, "poor"));
