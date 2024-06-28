13d16f1ca6a615255f42904c3118b71f5231c453
AndriiKot <a_kocyuba@ukr.net>
2024-06-28 13:38:42 +0200

    JavaScript CodeWars 8-kyu ! 'Leonardo Dicaprio and Oscars' !

---
+++ kyu-8/_243__Leonardo_Dicaprio_and_Oscars/solution.js
@@ -0,0 +1,8 @@
+function leo(oscar) {
+  let res = "";
+  if (oscar === 88) res = "Leo finally won the oscar! Leo is happy";
+  else if (oscar === 86) res = "Not even for Wolf of wallstreet?!";
+  else if (oscar < 88) res = "When will you give Leo an Oscar?";
+  else res = "Leo got one already!";
+  return res;
+}
