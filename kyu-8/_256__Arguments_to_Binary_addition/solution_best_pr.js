66a65b4df332953f74e6182a3cc97952920d40aa
AndriiKot <a_kocyuba@ukr.net>
2024-07-21 23:40:55 +0200

    JavaScript CodeWars 8-kyu 'Arguments to Binary addition'

---
+++ kyu-8/_256__Arguments_to_Binary_addition/solution_best_pr.js
@@ -0,0 +1,19 @@
+function arr2bin1(arr) {
+  return arr
+    .reduce((x, y) => x + (typeof y == "number" ? y : 0), 0)
+    .toString(2);
+}
+
+// My solution
+function arr2bin2(arr) {
+  let result = 0;
+  if (arr.length) {
+    result = arr.reduce((total, current) => {
+      if (typeof current === "number") {
+        return total + current;
+      }
+      return total;
+    }, 0);
+  }
+  return result.toString(2);
+}
