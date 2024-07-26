818f903e3daebdca7d6adad14fa4b877fe22dbc5
AndriiKot <a_kocyuba@ukr.net>
2024-07-26 12:59:05 +0200

    JavaScript CodeWars 8-kyu 'Training js #18 Methods of String Object concat split and it is good friedn join '

---
+++ kyu-8/_267__Training_JS__18__Methods_of_String_object_concat__split__and_its_good_friend_join/solution_best_pr.js
@@ -0,0 +1,19 @@
+// Codewars solution
+function splitAndMerge(string, separator) {
+  return string
+    .split(" ")
+    .map((el) => el.split("").join(separator))
+    .join(" ");
+}
+
+// My solution
+function splitAndMerge(string, separator) {
+  const arrayWords = string.split(" ");
+  let newString = "";
+  arrayWords.forEach((element) => {
+    console.log(element);
+    newString += element.split("").join(separator) + " ";
+  });
+
+  return newString.trim();
+}
