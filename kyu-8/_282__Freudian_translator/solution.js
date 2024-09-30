8c3b59bfc1c8f6a4ee2095d06b11cb8821fd5d93
AndriiKot <a_kocyuba@ukr.net>
2024-09-30 22:36:44 +0200

    JavaScript CodeWars 8-kyu 'Freudian translator'

---
+++ kyu-8/_282__Freudian_translator/solution.js
@@ -0,0 +1,9 @@
+function toFreud(string) {
+  let result = "";
+  if (string) {
+    result = string.split(" ");
+    result.map((_w, i, arr) => (arr[i] = "sex"));
+    result = result.join(" ");
+  }
+  return result;
+}
---
+++ kyu-8/_282__Freudian_translator/solution_best_pr.js
@@ -0,0 +1,13 @@
+// Best Praxes
+var toFreud1 = (s) => s.replace(/[^ ]+/g, "sex");
+
+// My Solution
+function toFreud2(string) {
+  let result = "";
+  if (string) {
+    result = string.split(" ");
+    result.map((_w, i, arr) => (arr[i] = "sex"));
+    result = result.join(" ");
+  }
+  return result;
+}
