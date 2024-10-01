0c95a2a5ea0b212e6881cd452f69ddab1ea9be29
AndriiKot <a_kocyuba@ukr.net>
2024-10-01 11:13:39 +0200

    JavaScript CodeWars 8-kyu 'Coding 3min : Jumping Dutch act'

---
+++ kyu-8/_283__Coding_3min__Jumping_Dutch_act/solution_best_pr.js
@@ -0,0 +1,22 @@
+function sc(floor) {
+  let result = "";
+  const constructorArray = (len, el) => Array.from({ length: len }, () => el);
+
+  if (floor > 1) {
+    let arr;
+    if (floor < 7) {
+      arr = constructorArray(floor + 1, "Aa~");
+      arr[arr.length - 2] = "Pa!";
+      arr[arr.length - 1] = "Aa!";
+    } else {
+      arr = constructorArray(floor, "Aa~");
+      arr[arr.length - 1] = "Pa!";
+    }
+    result = arr.join(" ");
+  }
+  return result;
+}
+
+console.log(sc(7)); // Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Pa!
+console.log(sc(6)); // Aa~ Aa~ Aa~ Aa~ Aa~ Pa! Aa!
+console.log(sc(10)); // Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Pa!
