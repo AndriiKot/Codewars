c74bcbd12db39f75dd048bba610be97270ec50cc
AndriiKot <a_kocyuba@ukr.net>
2024-10-06 12:34:56 +0200

    JavaScript CodeWars 8-kyu 'Take on Arrow style Functions'

---
+++ kyu-8/_289__Arrow_style_Functions/solution_best_pr.js
@@ -0,0 +1,13 @@
+// Best Praxe
+{
+  const ArrowFunc = (arr) => String.fromCharCode(...arr);
+}
+
+{
+  var ArrowFunc = (arr) => String.fromCharCode.apply(String, arr);
+}
+
+// My Solution
+const ArrowFunc = (arr) => {
+  return arr.map((el) => String.fromCharCode(el)).join(""); //Complete this function
+};
