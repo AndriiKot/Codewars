fedbde66b8d421e013dc192e37bf4c6fe9074c22
AndriiKot <a_kocyuba@ukr.net>
2024-10-18 14:39:47 +0200

    JavaScript CodeWars 8-kyu 'Crader' and 'Calculate Price Excluding VAT'

---
+++ kyu-8/_304__Calculate_Price_Excluding_VAT/solution_best_pr.js
@@ -0,0 +1,28 @@
+// Best Practice
+{
+  excludingVatPrice = (p) => (p === null ? -1 : +(p / 1.15).toFixed(2));
+}
+
+{
+  excludingVatPrice = (price) =>
+    price == null ? -1 : +(price / 1.15).toFixed(2);
+}
+
+// My Solution
+{
+  //return price without vat
+  function excludingVatPrice(price) {
+    let result = 0;
+    if (price > 0) {
+      const vat = (price / 115) * 15;
+      result = price - vat;
+      result = Number.parseFloat(result.toFixed(2));
+    }
+    if (price === null) result = -1;
+    return result;
+  }
+
+  // Testing
+  console.log(excludingVatPrice(230));
+  console.log(excludingVatPrice(123));
+}
