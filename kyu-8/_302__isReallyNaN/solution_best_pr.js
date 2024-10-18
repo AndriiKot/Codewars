b1d31a4634d833465fe80473524d6aaee8ce7cdc
AndriiKot <a_kocyuba@ukr.net>
2024-10-18 14:05:36 +0200

    JavaScript CodeWars 8-kyu 'Crader' and 'Switch Case Bug Fixing 6'

--- kyu-8/_302__isReallyNaN/solution_best_pr.js
+++ kyu-8/_302__isReallyNaN/solution_best_pr.js
@@ -1,18 +1,7 @@
-function grader(score) {
-  let result = "F";
-  switch (true) {
-    case 0.9 <= score && 1 >= score:
-      result = "A";
-      break;
-    case 0.8 <= score && 0.9 > score:
-      result = "B";
-      break;
-    case 0.7 <= score && 0.8 > score:
-      result = "C";
-      break;
-    case 0.6 <= score && 0.7 > score:
-      result = "D";
-      break;
+const isReallyNaN = (val) => {
+  let result = false;
+  if (typeof val === "number" && isNaN(val)) {
+    result = true;
   }
   return result;
-}
+};
