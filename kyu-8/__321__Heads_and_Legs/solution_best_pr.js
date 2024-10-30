c2502542889ac99b1b6b4ad7bdebc999aa56f7fd
AndriiKot <a_kocyuba@ukr.net>
2024-10-30 23:24:53 +0100

    JavaScript CodeWars 8-kyu 'Semi-Optional'

--- kyu-8/__321__Heads_and_Legs/solution_best_pr.js
+++
@@ -1,6 +0,0 @@
-function cutIt(arr) {
-  const minWords = arr.reduce((a, b) => (a.length < b.length ? a : b));
-  const lenMinWords = minWords.length;
-  const newArr = arr.map((word) => word.slice(0, lenMinWords));
-  return newArr;
-}
