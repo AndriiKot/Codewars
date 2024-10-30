7e9d7aaf8582a8be252d61f09116436a9b657d6f
AndriiKot <a_kocyuba@ukr.net>
2024-10-30 23:17:15 +0100

    JavaScript CodeWars 8-kyu 'Barking mad'

---
+++ kyu-8/_322__Barking_mad/solution.js
@@ -0,0 +1,13 @@
+class Dog {
+  constructor(breed) {
+    this.breed = breed;
+  }
+
+  bark() {
+    return "Woof";
+  }
+}
+
+const snoopy = new Dog("Beagle");
+
+const scoobydoo = new Dog("Great Dane");
