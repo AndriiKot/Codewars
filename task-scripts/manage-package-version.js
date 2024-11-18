ca3e73351bb261ec52800971279fe8f9ce4fc3bc
AndriiKot <a_kocyuba@ukr.net>
2024-11-18 18:53:07 +0100

    SQL CodeWars 'update script 'init npm testing script 5'

--- task-scripts/manage-package-version.js
+++ task-scripts/manage-package-version.js
@@ -1,21 +1,36 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { execSync } = require('node:child_process');
+import fs from "node:fs";
+import path from "node:path";
+import { execSync } from "node:child_process";
 
-const packageJsonPath = path.join(__dirname, '../package.json');
-const versionTag = execSync('git describe --tags --abbrev=0').toString().trim();
+const packageJsonPath = path.join(process.cwd(), "../package.json");
 
-if (!versionTag || !/^version_(\d+)_([\d]+)_([\d]+)$/.test(versionTag)) {
-  console.error('Tag must be in the format version_X_Y_Z');
-  process.exit(1);
+try {
+  const versionTag = execSync("git describe --tags --abbrev=0")
+    .toString()
+    .trim();
+
+  // Проверка на пустую строку или неправильный формат
+  if (!versionTag || !/^version_(\d+)_([\d]+)_([\d]+)$/.test(versionTag)) {
+    console.error("Tag must be in the format version_X_Y_Z");
+    process.exit(1);
+  }
+
+  const newVersion = versionTag.replace("version_", "").replace(/_/g, ".");
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+
+  if (packageJson.version !== newVersion) {
+    packageJson.version = newVersion;
+    fs.writeFileSync(
+      packageJsonPath,
+      JSON.stringify(packageJson, null, 2) + "\n",
+      "utf-8"
+    );
+    execSync("git add package.json");
+    console.log(`\x1b[32m\n
+Updated package.json version to ${newVersion}\n\x1b[0m`);
+  } else {
+    console.log("Version is already up to date.");
+  }
+} catch (error) {
+  console.error("An error occurred:", error.message);
 }
-
-const newVersion = versionTag.replace('version_', '').replace(/_/g, '.');
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
-
-if (!(packageJson.version === newVersion)) {
-  packageJson.version = newVersion;
-  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n', 'utf-8');
-  execSync('git add package.json');
-  console.log(`\x1b[32m\nUpdated package.json version to ${newVersion}\n\x1b[0m`);
-} esle { console.log('TESTING script');
