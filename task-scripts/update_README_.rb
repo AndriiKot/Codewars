d259dad9ecf6511cebee8a0abe2780c4d7384c46
AndriiKot <AndriiKot@users.noreply.github.com>
2025-01-15 14:56:29 +0100

    Python CodeWars kyu-8  ! 'Event Or Odd ' !

--- task-scripts/update_README_.rb
+++ task-scripts/update_README_.rb
@@ -1,7 +1,7 @@
 ﻿require 'net/http'
 require 'json'
 
-url = URI.parse('https://.codewars.com/api/v1/users/AndriiKot')
+url = URI.parse('https://codewars.com/api/v1/users/AndriiKot')
 http = Net::HTTP.new(url.host, url.port)
 http.use_ssl = (url.scheme == 'https')
 
@@ -13,22 +13,21 @@ if response.code == '200'
   res = response.body
 
   data = JSON.parse(res)
-  
+
   user_name = data['username']
-  
+
   template = "# [#{user_name}](https://www.codewars.com/users/AndriiKot) \n"
 
   hash_languages = data["ranks"]["languages"]
 
-  kyu_7, kyu_8 = "./kyu-7/", "./kyu-8/"
-  folders_count_7 = Dir.glob("#{kyu_7}/*").count
+  kyu_8 = "./kyu-8/"
   folders_count_8 = Dir.glob("#{kyu_8}/*").count
 
   time = Time.now
 
   hash_languages.each do |key, value|
     if key == 'python'
-      template += <<~EOF  
+      template += <<~EOF
         ## Python
 
         ##### As of #{time.strftime("%Y-%m-%d %H:%M:%S")} uploaded:
