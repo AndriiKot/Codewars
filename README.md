b18759525fa1568904c88215c18fb74bfbe73813
AndriiKot <AndriiKot@users.noreply.github.com>
2025-01-24 21:18:29 +0100

    Ruby CodeWars update task-scripts ! add kyu-6 !

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # [AndriiKot](https://www.codewars.com/users/AndriiKot) 
 ## Ruby
 
-##### As of 2025-01-24 21:14:35 uploaded:
+##### As of 2025-01-24 21:18:30 uploaded:
 
 #### Ruby rank: 7 kyu
 
 #### Ruby score: 55
 
-##### [7-kyu: 9](https://github.com/AndriiKot/Ruby__CodeWars/tree/main/kyu-7)
+#### [6-kyu: 1](https://github.com/AndriiKot/Ruby__CodeWars/tree/main/kyu-6)
 
-##### [8-kyu: 10](https://github.com/AndriiKot/Ruby__CodeWars/tree/main/kyu-8)
+##### [7-kyu: 10](https://github.com/AndriiKot/Ruby__CodeWars/tree/main/kyu-7)
+
+##### [8-kyu: 1](https://github.com/AndriiKot/Ruby__CodeWars/tree/main/kyu-8)
 
--- task-scripts/update_README_.rb
+++ task-scripts/update_README_.rb
@@ -20,7 +20,8 @@ if response.code == '200'
 
   hash_languages = data["ranks"]["languages"]
 
-  kyu_7, kyu_8 = "./kyu-7/", "./kyu-8/"
+  kyu_6, kyu_7, kyu_8 = "./kyu-7/", "./kyu-8/", "./kyu-6"
+  folders_count_6 = Dir.glob("#{kyu_6}").count
   folders_count_7 = Dir.glob("#{kyu_7}/*").count
   folders_count_8 = Dir.glob("#{kyu_8}/*").count
 
@@ -37,6 +38,8 @@ if response.code == '200'
         #### Ruby rank: #{value['name']}
 
         #### Ruby score: #{value['score']}
+  
+        #### [6-kyu: #{folders_count_6}](https://github.com/AndriiKot/Ruby__CodeWars/tree/main/kyu-6)
 
         ##### [7-kyu: #{folders_count_7}](https://github.com/AndriiKot/Ruby__CodeWars/tree/main/kyu-7)
 
