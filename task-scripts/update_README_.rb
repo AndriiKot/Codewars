ed7a526c213e53d814ffa39519b544b0f9597cd1
AndriiKot <AndriiKot@users.noreply.github.com>
2025-01-24 23:51:45 +0100

    Ruby CodeWars update task-scripts ! add kyu-5 bag fix !

--- task-scripts/update_README_.rb
+++ task-scripts/update_README_.rb
@@ -20,7 +20,7 @@ if response.code == '200'
 
   hash_languages = data["ranks"]["languages"]
 
-  kyu_5, kyu_6, kyu_7, kyu_8 = "./kyu_5/", "./kyu-6/", "./kyu-7/", "./kyu-8/"
+  kyu_5, kyu_6, kyu_7, kyu_8 = "./kyu-5/", "./kyu-6/", "./kyu-7/", "./kyu-8/"
   folders_count_5 = Dir.glob("#{kyu_5}").count
   folders_count_6 = Dir.glob("#{kyu_6}").count
   folders_count_7 = Dir.glob("#{kyu_7}/*").count
