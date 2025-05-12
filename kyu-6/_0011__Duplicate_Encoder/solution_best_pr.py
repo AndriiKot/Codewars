3d9dfd5ed6ed958f7f9c16ea90f8080df2d87cc1
AndriiKot <AndriiKot@users.noreply.github.com>
2025-05-12 11:40:54 +0200

    JavaScript CodeWars kyu-6 ! 'Take a Ten Minutes Walk' !

--- kyu-6/_0011__Duplicate_Encoder/solution_best_pr.py
+++
@@ -1,7 +0,0 @@
-function duplicateEncode(word, res = ''){
-  word = word.toLowerCase();
-  for (let i = 0; i < word.length; i++) {
-    res += word.split(word[i]).length - 1 == 1 ? '(' : ')'
-  };
-  return res;
-}
