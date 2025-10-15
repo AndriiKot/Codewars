2f4f5efa0dac1915c74b8a25c552b3178013aa4f
AndriiKot <AndriiKot@users.noreply.github.com>
2025-10-15 23:10:12 +0200

    Python CodeWars kyu-8 ! 'How many lightsabers do you own?' !

---
+++ kyu-8/_151__how-many-lightsabers-do-you-own/solution_best_pr.py
@@ -0,0 +1,9 @@
+def how_many_light_sabers_do_you_own(name=''):
+    return 18*(name == 'Zach')
+
+def how_many_light_sabers_do_you_own(name=''):
+    return 18 if name == 'Zach' else 0
+
+def how_many_light_sabers_do_you_own(name=None):
+    return 18 if name == "Zach" else 0
+
